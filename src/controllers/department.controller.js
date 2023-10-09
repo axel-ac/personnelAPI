fc079c221ba23edc23a549bd73db085cc4d07564
axel-ac <abdullahceylan.axel@gmail.com>
2023-10-09 19:58:59 +0200

    department.model.controller

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -1,4 +1,56 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const Departman = require("../models/department.model");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(Department);
+
+    res.status(200).send({
+      error: false,
+      data, // data: data
+    });
+  },
+  create: async (req, res) => {
+
+    const data = await Department.create(req.body)
+
+      res.status(201).send({
+        error: false,
+        data
+      });
+
+  },
+  read: async (req, res) => {
+await Department.findOne({_id: req.params.id})
+
+  res.status(200).send({
+    error: false,
+    data
+  });
+
+  },
+  update: async (req, res) => {
+
+    const data = await Department.updateOne({_id: req.params.id}, req.body)
+
+     res.status(202).send({
+       error: false,
+       data,
+       new: await Department.findOne({ _id: req.params.id }),
+     });
+
+  },
+  delete: async (req, res) => {
+
+    const data = await Department.deleteOne({ _id: req.params.id });
+    
+  res.status(data.deletedCount >= 1 ? 204 : 404).send({
+    error: false,
+    data,
+  });
+  },
+};
--- src/models/department.model.js
+++ src/models/department.model.js
@@ -1,6 +1,25 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+
+const DepartmenSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    collection: "departments",
+    timestamps: true,
+  }
+);
+
+/* ------------------------------------------------------- */
+
+module.exports = mongoose.model("Department", DepartmentSchema);
