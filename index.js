95d05c5fafa11eba81636745a6cb2b15ddddb9d1
axel-ac <abdullahceylan.axel@gmail.com>
2023-10-09 20:43:35 +0200

    per.router-index

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ app.all("/", (req, res) => {
 
 // departments
 app.use("/departments", require("./src/routes/department.router"));
-
+// /personnels
+app.use('/personnels', require('./src/routes/personnel.router'))
 
 
 /* ------------------------------------------------------- */
